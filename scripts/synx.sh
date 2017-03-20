2cadd68bbbaef0de7d3c3426ee6e4d2444290eeb
Luc Dion <Luc.DION@bdc.ca>
2017-03-19 20:34:26 -0400

    update for travis

--- scripts/synx.sh
+++ scripts/synx.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-synx --no-sort-by-name PAP.xcodeproj
-
-echo 
-echo 
-echo =====================================================
-echo List of empty forlders
-echo =====================================================
-find ./PAP -depth -type d -empty
-echo end
+synx --no-sort-by-name MCSwiftLayout.xcodeproj

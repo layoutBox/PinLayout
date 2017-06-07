a2a9e176855c944a5f2c400a7953a4f23b0c88aa
Luc Dion <Luc.DION@bdc.ca>
2017-06-07 07:01:21 -0400

    Initial commit

--- Sources/Coordinates.swift
+++ Sources/Coordinates.swift
@@ -25,7 +25,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-import UIKit
+import Foundation
 
 class Coordinates {
     static func hCenter(_ view: UIView) -> CGFloat {
--- Sources/PinLayout.swift
+++ Sources/PinLayout.swift
@@ -25,7 +25,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-import UIKit
+import Foundation
 
 // MARK: - PinLayout UIView's extension
 public extension UIView {

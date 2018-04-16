aa3bc0bf9471dd6927c30f5fd812d7e7df2b477a
Luc Dion <ldion@mirego.com>
2018-04-16 08:19:05 -0400

    Add macOS unit tests + move iOS tests inside "iOS" folder.

---
+++ Tests/iOS/AspectRatioTests.swift
@@ -0,0 +1,239 @@
+//  Copyright (c) 2017 Luc Dion
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Quick
+import Nimble
+import PinLayout
+
+class AspectRatioTests: QuickSpec {
+    override func spec() {
+        var viewController: UIViewController!
+        var rootView: BasicView!
+        var aView: BasicView!
+        var bView: BasicView!
+        var imageView: UIImageView!
+        
+        /*
+         root
+         |
+         - aView
+         - bView
+         - imageView
+         */
+        
+        beforeSuite {
+            _pinlayoutSetUnitTest(displayScale: 2)
+        }
+        
+        beforeEach {
+            Pin.lastWarningText = nil
+            
+            viewController = UIViewController()
+            
+            rootView = BasicView(text: "", color: .white)
+            rootView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+            viewController.view.addSubview(rootView)
+            
+            aView = BasicView(text: "View A", color: UIColor.red.withAlphaComponent(0.5))
+            aView.frame = CGRect(x: 140, y: 100, width: 200, height: 100)
+            rootView.addSubview(aView)
+            
+            bView = BasicView(text: "View A", color: UIColor.red.withAlphaComponent(0.5))
+            bView.frame = CGRect(x: 160, y: 120, width: 120, height: 40)
+            rootView.addSubview(bView)
+            
+            imageView = UIImageView(image: UIImage(color: UIColor.red, size: CGSize(width: 100, height: 50)))
+            imageView.frame = CGRect(x: 10, y: 10, width: 60, height: 60)
+            rootView.addSubview(imageView)
+        }
+        
+        //
+        // aspectRatio(: CGFloat)
+        //
+        describe("the result of the aspectRatio(CGFloat)") {
+            it("should warn about fitSize()") {
+                aView.pin.left().width(100%).aspectRatio(2).fitSize()
+                expect(Pin.lastWarningText).to(contain(["fitSize() won't be applied", "conflicts", "aspectRatio"]))
+            }
+            
+            it("should warn about fitSize()") {
+                aView.pin.left().height(100).aspectRatio(2).sizeToFit(.width)
+                expect(Pin.lastWarningText).to(contain(["sizeToFit(.width) won't be applied", "conflicts", "aspectRatio"]))
+            }
+            
+            it("should warn about aspectRatio(:CGFloat)") {
+                aView.sizeThatFitsExpectedArea = 40 * 40
+                aView.pin.left().width(100%).sizeToFit(.width).aspectRatio(2)
+                expect(Pin.lastWarningText).to(contain(["aspectRatio(2.0) won't be applied", "conflicts", "sizeToFit"]))
+            }
+
+            it("should warn about aspectRatio(:CGFloat)") {
+                aView.sizeThatFitsExpectedArea = 40 * 40
+                aView.pin.left().height(100).sizeToFit(.height).aspectRatio(2)
+                expect(Pin.lastWarningText).to(contain(["aspectRatio(2.0) won't be applied", "conflicts", "sizeToFit"]))
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 16.0, height: 100.0)))
+            }
+            
+            it("should warn about aspectRatio(:CGFloat)") {
+                aView.pin.left().aspectRatio(2)
+                expect(Pin.lastWarningText).to(contain(["aspectRatio won't be applied", "neither the width nor the height can be determined"]))
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 200.0, height: 100.0)))
+            }
+            
+            it("should warn about aspectRatio(:CGFloat)") {
+                aView.pin.left().width(100%).aspectRatio(-2)
+                expect(Pin.lastWarningText).to(contain(["aspectRatio", "won't be applied", "must be greater than zero"]))
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 400.0, height: 100.0)))
+            }
+            
+            it("should warn about aspectRatio(:CGFloat)") {
+                aView.pin.left().height(100).aspectRatio(-2)
+                expect(Pin.lastWarningText).to(contain(["aspectRatio", "won't be applied", "must be greater than zero"]))
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 200.0, height: 100.0)))
+            }
+            
+            it("should warn about aspectRatio(:CGFloat)") {
+                aView.pin.left().width(100%).aspectRatio(0)
+                expect(Pin.lastWarningText).to(contain(["aspectRatio", "won't be applied", "must be greater than zero"]))
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 400.0, height: 100.0)))
+            }
+            
+            it("should warn about aspectRatio(:CGFloat)") {
+                aView.pin.left().height(100).aspectRatio(0)
+                expect(Pin.lastWarningText).to(contain(["aspectRatio", "won't be applied", "must be greater than zero"]))
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 200.0, height: 100.0)))
+            }
+
+            it("should apply aspectRatio(:CGFloat)") {
+                aView.pin.left().width(100%).aspectRatio(2)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 400.0, height: 200.0)))
+            }
+            
+            it("should apply aspectRatio(:CGFloat)") {
+                aView.pin.left().height(100).aspectRatio(2)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 200.0, height: 100.0)))
+            }
+            
+            it("should apply aspectRatio(:CGFloat)") {
+                aView.pin.left().width(100).aspectRatio(0.5)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 100.0, height: 200.0)))
+            }
+            
+            it("should apply aspectRatio(:CGFloat)") {
+                aView.pin.left().height(100).aspectRatio(0.5)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 50.0, height: 100.0)))
+            
+            }
+        }
+        
+        //
+        // aspectRatio(of: UIView)
+        //
+        describe("the result of the aspectRatio(of: UIView)") {
+            it("should apply aspectRatio(of: UIView)") {
+                aView.pin.left().width(100).aspectRatio(of: bView)
+                let bViewRatio = bView.frame.width / bView.frame.height
+                expect(aView.frame).to(beCloseTo(CGRect(x: 0.0, y: 100.0, width: 100.0, height: 100 / bViewRatio), within: 0.5))
+            }
+            
+            it("should apply aspectRatio()") {
+                aView.pin.left().height(100).aspectRatio(of: bView)
+                let bViewRatio = bView.frame.width / bView.frame.height
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 100 * bViewRatio, height: 100.0)))
+            }
+        }
+        
+        //
+        // aspectRatio()
+        //
+        describe("the result of the aspectRatio(CGFloat)") {
+            it("should warn about aspectRatio()") {
+                aView.pin.left().width(100).aspectRatio()
+                expect(Pin.lastWarningText).to(contain(["aspectRatio() won't be applied", "the layouted must be an UIImageView()"]))
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 100.0, height: 100.0)))
+            }
+            
+            it("should warn about aspectRatio()") {
+                imageView.pin.left().aspectRatio()
+                expect(Pin.lastWarningText).to(contain(["aspectRatio won't be applied", "neither the width nor the height can be determined"]))
+                expect(imageView.frame).to(equal(CGRect(x: 0.0, y: 10.0, width: 60.0, height: 60.0)))
+            }
+            
+            it("should apply aspectRatio()") {
+                imageView.pin.left().width(200).aspectRatio()
+                expect(imageView.frame).to(beCloseTo(CGRect(x: 0.0, y: 10, width: 200, height: 100), within: 0.5))
+            }
+        }
+        
+        //
+        // aspectRatio && min/max width/height
+        //
+        describe("the result of the aspectRatio(CGFloat)") {
+            it("should apply aspectRatio()") {
+                aView.pin.left().height(100).aspectRatio(2).minWidth(240)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 240.0, height: 100.0)))
+            }
+            
+            it("should apply aspectRatio()") {
+                aView.pin.left().height(100).aspectRatio(2).maxWidth(140)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 140.0, height: 100.0)))
+            }
+            
+            it("should apply aspectRatio()") {
+                aView.pin.left().width(100).aspectRatio(2).minHeight(70)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 100.0, height: 70.0)))
+            }
+            
+            it("should apply aspectRatio()") {
+                aView.pin.left().width(100).aspectRatio(2).maxHeight(30)
+                    expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 100.0, height: 30.0)))
+            }
+
+            it("should apply aspectRatio()") {
+                aView.pin.left().width(100).maxWidth(80).aspectRatio(2)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 80.0, height: 40.0)))
+            }
+            
+            it("should apply aspectRatio()") {
+                aView.pin.left().width(100).minWidth(100).aspectRatio(2)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 100.0, height: 50.0)))
+            }
+            
+            it("should apply aspectRatio()") {
+                aView.pin.left().width(100).maxWidth(80).aspectRatio(2).maxHeight(30)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 80.0, height: 30.0)))
+            }
+            
+            it("should apply aspectRatio()") {
+                aView.pin.left().height(100).maxHeight(80).aspectRatio(2)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 160.0, height: 80.0)))
+            }
+            
+            it("should apply aspectRatio()") {
+                aView.pin.left().height(100).minHeight(120).aspectRatio(2)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 240.0, height: 120.0)))
+            }
+            
+            it("should apply aspectRatio()") {
+                aView.pin.left().height(100).maxHeight(80).aspectRatio(2).maxWidth(140)
+                expect(aView.frame).to(equal(CGRect(x: 0.0, y: 100.0, width: 140.0, height: 80.0)))
+            }
+        }
+    }
+}
---
+++ Tests/iOS/RelativePositionMultipleViewsSpec.swift
@@ -0,0 +1,330 @@
+//  Copyright (c) 2017 Luc Dion
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Quick
+import Nimble
+import PinLayout
+
+class RelativePositionMultipleViewsSpec: QuickSpec {
+    override func spec() {
+        var viewController: UIViewController!
+        
+        var rootView: UIView!
+        
+        var aView: UIView!
+        var aViewChild: UIView!
+        
+        var bView: UIView!
+        var bViewChild: UIView!
+        
+        /*
+          root
+           |
+            - aView
+           |    |
+           |     - aViewChild
+           |
+            - bView
+                |
+                 - bViewChild
+        */
+
+        beforeEach {
+            Pin.lastWarningText = nil
+            
+            viewController = UIViewController()
+            
+            rootView = UIView()
+            rootView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+            viewController.view.addSubview(rootView)
+            
+            aView = UIView()
+            aView.frame = CGRect(x: 40, y: 100, width: 100, height: 60)
+            rootView.addSubview(aView)
+            
+            aViewChild = UIView()
+            aViewChild.frame = CGRect(x: 10, y: 20, width: 50, height: 30)
+            aView.addSubview(aViewChild)
+            
+            bView = UIView()
+            bView.frame = CGRect(x: 160, y: 120, width: 110, height: 80)
+            rootView.addSubview(bView)
+            
+            bViewChild = UIView()
+            bViewChild.frame = CGRect(x: 40, y: 10, width: 60, height: 20)
+            bView.addSubview(bViewChild)
+        }
+        
+        //
+        // above(of: UIViews.....) warnings
+        //
+        describe("the result of above(of: UIView...) with multiple relative views") {
+            it("should warns the view bottom edge") {
+                let unatachedView = UIView()
+                bViewChild.pin.above(of: unatachedView)
+                expect(bViewChild.frame).to(equal(CGRect(x: 40, y: 10, width: 60, height: 20)))
+                expect(Pin.lastWarningText).to(contain(["above", "won't be applied", "no valid references"]))
+            }
+            
+            it("should warns the view bottom edge") {
+                let unatachedView = UIView()
+                bViewChild.pin.above(of: [aView, unatachedView])
+                expect(bViewChild.frame).to(equal(CGRect(x: 40.0, y: -40.0, width: 60.0, height: 20.0)))
+                expect(Pin.lastWarningText).to(contain(["above", "won't be applied", "the reference view", "must be added", "as a reference"]))
+            }
+            
+            it("Should warn, but the view should be anyway layout it above") {
+                let unatachedView = UIView()
+                bViewChild.pin.above(of: [aView, unatachedView])
+                expect(bViewChild.frame).to(equal(CGRect(x: 40.0, y: -40.0, width: 60.0, height: 20.0)))
+                expect(Pin.lastWarningText).to(contain(["above", "won't be applied", "the reference view", "must be added", "as a reference"]))
+            }
+        }
+        
+        //
+        // above(of: UIViews.....)
+        //
+        describe("the result of above(of: UIView...) with multiple relative views") {
+            it("should move the view above relative views") {
+                bViewChild.pin.above(of: aView)
+                expect(bViewChild.frame).to(equal(CGRect(x: 40.0, y: -40.0, width: 60.0, height: 20.0)))
+            }
+
+            it("should move the view above relative views") {
+                bViewChild.pin.above(of: [aView, bView])
+                expect(bViewChild.frame).to(equal(CGRect(x: 40.0, y: -40.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view above relative views") {
+                bViewChild.pin.above(of: [aView, bView], aligned: .left)
+                expect(bViewChild.frame).to(equal(CGRect(x: -120.0, y: -40.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view above relative views") {
+                bViewChild.pin.above(of: [aView, bView], aligned: .right)
+                expect(bViewChild.frame).to(equal(CGRect(x: 50.0, y: -40.0, width: 60.0, height: 20.0)))
+            }
+
+            it("should move the view above relative views") {
+                bViewChild.pin.above(of: aView, aligned: .center)
+                expect(bViewChild.frame).to(equal(CGRect(x: -100.0, y: -40.0, width: 60.0, height: 20.0)))
+            }
+
+            it("should move the view above relative views") {
+                bViewChild.pin.above(of: [aView, bView], aligned: .center)
+                expect(bViewChild.frame).to(equal(CGRect(x: -37.5, y: -40.0, width: 60.0, height: 20.0)))
+            }
+
+            it("should move the view above relative views") {
+                bViewChild.pin.above(of: [aView, bView], aligned: .right).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: 40.0, y: -50.0, width: 60.0, height: 20.0)))
+            }
+
+            it("should move the view above relative views") {
+                bViewChild.pin.above(of: [aView, bView], aligned: .left).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: -110.0, y: -50.0, width: 60.0, height: 20.0)))
+            }
+        }
+    
+        //
+        // below(of: UIViews.....)
+        //
+        describe("the result of below(of: UIView...) with multiple relative views") {
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: aView)
+                expect(bViewChild.frame).to(equal(CGRect(x: 40.0, y: 40.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: [aView, bView])
+                expect(bViewChild.frame).to(equal(CGRect(x: 40.0, y: 80.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: [aView, bView], aligned: .left)
+                expect(bViewChild.frame).to(equal(CGRect(x: -120.0, y: 80.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: [aView, bView], aligned: .right)
+                expect(bViewChild.frame).to(equal(CGRect(x: 50.0, y: 80.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: aView, aligned: .center)
+                expect(bViewChild.frame).to(equal(CGRect(x: -100.0, y: 40.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: [aView, bView], aligned: .center)
+                expect(bViewChild.frame).to(equal(CGRect(x: -37.5, y: 80.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: [aView, bView], aligned: .right).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: 40.0, y: 90.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: [aView, bView], aligned: .left).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: -110.0, y: 90.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: [aView, bView], aligned: .center).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: -37.5, y: 90.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: [aView, bView], aligned: .center).marginLeft(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: -27.5, y: 80.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view below relative views") {
+                bViewChild.pin.below(of: [aView, bView], aligned: .center).marginRight(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: -47.5, y: 80.0, width: 60.0, height: 20.0)))
+            }
+        }
+        
+        //
+        // left(of: UIViews.....)
+        //
+        describe("the result of left(of: UIView...) with multiple relative views") {
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: aView)
+                expect(bViewChild.frame).to(equal(CGRect(x: -180.0, y: 10.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: [aView, bView])
+                expect(bViewChild.frame).to(equal(CGRect(x: -180.0, y: 10.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: [bView, bView])
+                expect(bViewChild.frame).to(equal(CGRect(x: -60.0, y: 10.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: [aView, bView], aligned: .top)
+                expect(bViewChild.frame).to(equal(CGRect(x: -180.0, y: -20.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: [aView, bView], aligned: .bottom)
+                expect(bViewChild.frame).to(equal(CGRect(x: -180.0, y: 60.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: aView, aligned: .center)
+                expect(bViewChild.frame).to(equal(CGRect(x: -180.0, y: 0.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: [aView, bView], aligned: .center)
+                expect(bViewChild.frame).to(equal(CGRect(x: -180.0, y: 15.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: [aView, bView], aligned: .bottom).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: -190.0, y: 50.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: [aView, bView], aligned: .top).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: -190.0, y: -10.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: [aView, bView], aligned: .center).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: -190.0, y: 15.0, width: 60.0, height: 20.0)))
+            }
+
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: [aView, bView], aligned: .center).marginTop(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: -180.0, y: 25.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view left relative views") {
+                bViewChild.pin.left(of: [aView, bView], aligned: .center).marginBottom(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: -180.0, y: 5.0, width: 60.0, height: 20.0)))
+            }
+        }
+        
+        //
+        // right(of: UIViews.....)
+        //
+        describe("the result of right(of: UIView...) with multiple relative views") {
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: aView)
+                expect(bViewChild.frame).to(equal(CGRect(x: -20.0, y: 10.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: [aView, bView])
+                expect(bViewChild.frame).to(equal(CGRect(x: 110.0, y: 10.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: [aView, bView], aligned: .top)
+                expect(bViewChild.frame).to(equal(CGRect(x: 110.0, y: -20.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: [aView, bView], aligned: .bottom)
+                expect(bViewChild.frame).to(equal(CGRect(x: 110.0, y: 60.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: aView, aligned: .center)
+                expect(bViewChild.frame).to(equal(CGRect(x: -20.0, y: 0.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: [aView, bView], aligned: .center)
+                expect(bViewChild.frame).to(equal(CGRect(x: 110.0, y: 15.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: [aView, bView], aligned: .bottom).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: 120.0, y: 50.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: [aView, bView], aligned: .top).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: 120.0, y: -10.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: [aView, bView], aligned: .center).margin(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: 120.0, y: 15.0, width: 60.0, height: 20.0)))
+            }
+
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: [aView, bView], aligned: .center).marginTop(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: 110.0, y: 25.0, width: 60.0, height: 20.0)))
+            }
+            
+            it("should move the view right relative views") {
+                bViewChild.pin.right(of: [aView, bView], aligned: .center).marginBottom(10)
+                expect(bViewChild.frame).to(equal(CGRect(x: 110.0, y: 5.0, width: 60.0, height: 20.0)))
+            }
+        }
+    }
+}

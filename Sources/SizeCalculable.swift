46c7d9468a17f11fa39f5ba18c5f016ffaef7359
Antoine Lamy <antoinelamy@gmail.com>
2018-06-14 08:41:20 -0400

    Add missing imports

--- Sources/SizeCalculable.swift
+++ Sources/SizeCalculable.swift
@@ -6,7 +6,11 @@
 //  Copyright © 2018 mcswiftlayyout.mirego.com. All rights reserved.
 //
 
-import Foundation
+#if os(iOS) || os(tvOS)
+import UIKit
+#else
+import AppKit
+#endif
 
 public protocol SizeCalculable {
     func sizeThatFits(_ size: CGSize) -> CGSize

e3290a880b1cdc5de1d634a519efeb4dc53cffb3
Luc Dion <ldion@mirego.com>
2018-08-15 07:18:49 -0400

    Update

--- Sources/Extensions/PEdgeInsets+Operators.swift
+++ Sources/Extensions/PEdgeInsets+Operators.swift
@@ -1,26 +1,25 @@
 
 public extension PEdgeInsets {
     public func inset(_ value: CGFloat) -> PEdgeInsets {
-        return UIEdgeInsets(top: self.top + value,
+        return PEdgeInsets(top: self.top + value,
                             left: self.left + value,
                             bottom: self.bottom + value,
                             right: self.right + value)
     }
 
-    func insetBy(dx: CGFloat, dy: CGFloat) -> UIEdgeInsets {
-        return UIEdgeInsets(top: self.top + dy, left: self.left + dx, bottom: self.bottom + dy, right: self.right + dx)
+    func insetBy(dx: CGFloat, dy: CGFloat) -> PEdgeInsets {
+        return PEdgeInsets(top: self.top + dy, left: self.left + dx, bottom: self.bottom + dy, right: self.right + dx)
     }
 
-
-    func minInsets(_ insets: UIEdgeInsets) -> UIEdgeInsets {
-        return UIEdgeInsets(top: minValue(self.top, minValue: insets.top),
+    func minInsets(_ insets: PEdgeInsets) -> PEdgeInsets {
+        return PEdgeInsets(top: minValue(self.top, minValue: insets.top),
                             left: minValue(self.left, minValue: insets.left),
                             bottom: minValue(self.bottom, minValue: insets.bottom),
                             right: minValue(self.right, minValue: insets.right))
     }
 
-    func minInsets(dx: CGFloat, dy: CGFloat) -> UIEdgeInsets {
-        return UIEdgeInsets(top: minValue(self.top, minValue: dy),
+    func minInsets(dx: CGFloat, dy: CGFloat) -> PEdgeInsets {
+        return PEdgeInsets(top: minValue(self.top, minValue: dy),
                             left: minValue(self.left, minValue: dx),
                             bottom: minValue(self.bottom, minValue: dy),
                             right: minValue(self.right, minValue: dx))
@@ -30,10 +29,15 @@ public extension PEdgeInsets {
         return value >= minValue ? value : minValue
     }
 }
-//public func +(lhs: PEdgeInsets, rhs: Int) -> PEdgeInsets {
-//    return lhs
-//}
-//
+
+public func +(lhs: PEdgeInsets, rhs: Int) -> PEdgeInsets {
+    let rhsf = CGFloat(rhs)
+    return PEdgeInsets(top: lhs.top + rhsf,
+                       left: lhs.left + rhsf,
+                       bottom: lhs.bottom + rhsf,
+                       right: lhs.right + rhsf)
+}
+
 public func +(lhs: PEdgeInsets, rhs: CGFloat) -> PEdgeInsets {
     return PEdgeInsets(top: lhs.top + rhs,
                        left: lhs.left + rhs,

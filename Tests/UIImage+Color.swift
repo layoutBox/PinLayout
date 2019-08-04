7f44a9e63475f75d48f556a451a2e3529dc5641b
Luc Dion <luc@transit.app>
2019-08-03 21:00:52 -0400

    Update to Swift 5.

--- Tests/UIImage+Color.swift
+++ Tests/UIImage+Color.swift
@@ -20,7 +20,7 @@
 import UIKit
 
 public extension UIImage {
-    public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+    convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
         let rect = CGRect(origin: .zero, size: size)
         UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
         color.setFill()

21c44db04bce270efbd27cb4e2492a513818cc1e
hyun99999 <ypsophila97@icloud.com>
2023-10-18 13:31:43 +0900

    Use UIApplication instead of UIScreen.main on iOS 13.0 and later

--- Sources/Impl/Coordinates.swift
+++ Sources/Impl/Coordinates.swift
@@ -108,7 +108,12 @@ final class Coordinates<PinView: Layoutable> {
 
 private func getDisplayScale() -> CGFloat {
     #if os(iOS) || os(tvOS)
+    if #available(iOS 13.0, *) {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        return windowScene?.screen.scale ?? .zero
+    } else {
         return UIScreen.main.scale
+    }
     #elseif os(OSX)
         #if swift(>=4.1)
         return NSScreen.main?.backingScaleFactor ?? 2.0
